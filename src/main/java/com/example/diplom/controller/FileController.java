d4c657d31107c4fa2b7218452ebd0fac1c8414c9
Artem <artemvalt12@gmail.com>
2024-02-04 21:13:01 +0400

    update download logic

--- src/main/java/com/example/diplom/controller/FileController.java
+++ src/main/java/com/example/diplom/controller/FileController.java
@@ -6,12 +6,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/file")
@@ -43,15 +37,14 @@ public class FileController {
             @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema())}),
             @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     public ResponseEntity<Resource> downloadFile(@PathVariable Long dealId, WebRequest webreuest) throws IOException {
-        String path = "/Users/belousovartem/IdeaProjects/bdcourse/Diplom/dq.docx";
-        File file = new File(path);
-        MediaType pdf = MediaType.APPLICATION_PDF;
+
+        File file = fileService.getFileWithoutBookMarks(Map.of("замена", "qdqwhdqhwdhqwiod"));
         HttpHeaders header = new HttpHeaders();
         header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=dq.docx");
         header.add("Cache-Control", "no-cache, no-store, must-revalidate");
         header.add("Pragma", "no-cache");
         header.add("Expires", "0");
-        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Path.of(path)));
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Path.of(file.getAbsolutePath())));
 
         return ResponseEntity.ok()
                 .headers(header)
--- src/main/java/com/example/diplom/service/FileService.java
+++ src/main/java/com/example/diplom/service/FileService.java
@@ -3,13 +3,9 @@ package com.example.diplom.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.xwpf.converter.pdf.PdfConverter;
-import org.apache.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTBookmark;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -20,67 +16,54 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 public class FileService {
-    public void getFileWithoutBookMarks(Map<String, String> bookmarks) {
-        try {
-//            XWPFDocument document = new XWPFDocument();
-//            runParagraphs(bookmarks, document);
-//            return getNewFile(document);
-            convertToPDF("/Users/belousovartem/IdeaProjects/bdcourse/Diplom/dq.docx", "/Users/belousovartem/IdeaProjects/bdcourse/Diplom/test.pdf");
+    private final String PATH = "/Users/belousovartem/IdeaProjects/bdcourse/Diplom/dq12.docx";
+    private final String DELIMITER = "&";
 
+    public File getFileWithoutBookMarks(Map<String, String> bookmarks) {
+        try {
+            return getFile(bookmarks);
         } catch (Exception e) {
-            log.info(e.getLocalizedMessage());
+            log.error(e.getLocalizedMessage());
             throw new RuntimeException();
         }
     }
 
-    private XWPFDocument getNewFile(XWPFDocument document) throws IOException {
-        writeDataToFile(document);
-        return document;
+    private File getFile(Map<String, String> bookmarks) throws IOException {
+        saveFileWithBookmarks(bookmarks);
+        return new File(PATH);
     }
 
-    private void writeDataToFile(XWPFDocument document) throws IOException {
-        OutputStream out = new FileOutputStream("/Users/belousovartem/IdeaProjects/bdcourse/Diplom/dq.docx");
-        document.write(out);
-        document.close();
-        out.close();
-        convertToPDF("/Users/belousovartem/IdeaProjects/bdcourse/Diplom/dq.docx", "/Users/belousovartem/IdeaProjects/bdcourse/Diplom/test.pdf");
-    }
-
-    private void runParagraphs(Map<String, String> bookmarks, XWPFDocument document) {
+    private void saveFileWithBookmarks(Map<String, String> bookmarks) throws IOException {
+        InputStream fis = new FileInputStream(PATH);
+        XWPFDocument document = new XWPFDocument(fis);
         List<XWPFParagraph> paragraphs = document.getParagraphs();
-        for (XWPFParagraph paragraph : paragraphs) {
-            List<CTBookmark> bookmarkStartList = getCtBookmarks(paragraph);
-            bookmarkStartList.forEach(bookmark -> insertValueBookmark(bookmarks, paragraph));
-        }
+        paragraphs.forEach(paragraph -> updateParagraph(bookmarks, paragraph));
+        saveDataInDoc(document);
     }
 
-    private List<CTBookmark> getCtBookmarks(XWPFParagraph paragraph) {
-        CTP ctp = paragraph.getCTP();
-        return ctp.getBookmarkStartList();
+    private void updateParagraph(Map<String, String> bookmarks, XWPFParagraph paragraph) {
+        paragraph.getCTP().getBookmarkStartList()
+                .stream()
+                .map(bookmark -> paragraph.getText())
+                .forEach(paraText -> updateRowWithBookMark(bookmarks, paragraph, paraText));
     }
 
-    private void insertValueBookmark(Map<String, String> bookmarks, XWPFParagraph paragraph) {
-        String paraText = paragraph.getText();
-        String replacedText = getParaBookmark(bookmarks, paraText);
+    private void updateRowWithBookMark(Map<String, String> bookmarks, XWPFParagraph paragraph, String paraText) {
+        String replacedText = getReplacedText(bookmarks, paraText);
         removeAllRuns(paragraph);
         insertReplacementRuns(paragraph, replacedText);
     }
 
-    private String getParaBookmark(Map<String, String> bookmarks, String paraText) {
-        String bookmarkName = StringUtils.substringBetween(paraText, "&", "&");
-        return paraText.replaceAll("\\b&" + bookmarkName + "&\\b", bookmarks.get(bookmarkName));
+    private String getReplacedText(Map<String, String> bookmarks, String paraText) {
+        String bookmarkName = StringUtils.substringBetween(paraText, DELIMITER, DELIMITER);
+        return paraText.replaceAll(DELIMITER + bookmarkName + DELIMITER, bookmarks.get(bookmarkName));
     }
 
-    private void convertToPDF(String docPath, String pdfPath) {
-        try {
-            InputStream doc = new FileInputStream(docPath);
-            XWPFDocument document = new XWPFDocument(doc);
-            PdfOptions options = PdfOptions.create();
-            OutputStream out = new FileOutputStream(pdfPath);
-            PdfConverter.getInstance().convert(document, out, options);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+    private void saveDataInDoc(XWPFDocument document) throws IOException {
+        OutputStream out = new FileOutputStream(PATH);
+        document.write(out);
+        document.close();
+        out.close();
     }
 
     private void removeAllRuns(XWPFParagraph paragraph) {
